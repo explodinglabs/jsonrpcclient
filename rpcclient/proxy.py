ba0a4c3b0f18fb333333c22a66e5a5bcb02e236c
Beau Barker <beauinmelbourne@gmail.com>
2014-11-02 23:56:23 +1100

    Moving files around. Started setup.py

--- rpcclient/proxy.py
+++ rpcclient/proxy.py
@@ -83,7 +83,8 @@ class Proxy:
                 try:
                     jsonschema.validate(
                         response_dict,
-                        json.loads(open(os.path.dirname(__file__)+'/jsonrpc-2.0-response-schema.json').read()))
+                        json.loads(open(os.path.dirname(__file__)+ \
+                            '/response-schema.json').read()))
 
                 except jsonschema.ValidationError:
                     raise exceptions.InvalidResponse()
