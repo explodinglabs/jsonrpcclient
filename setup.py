b19f9c63b60584c19629b95098d90a80a9a76815
Beau Barker <beauinmelbourne@gmail.com>
2014-11-06 11:27:00 +1100

    setup.py: Updated to version 1.0.2

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 """setup.py"""
+#pylint:disable=line-too-long
 
 from distutils.core import setup
 
 setup(
-    name = 'jsonrpcclient',
-    packages = ['jsonrpcclient'],
+    name='jsonrpcclient',
+    packages=['jsonrpcclient'],
     package_data={'jsonrpcclient': ['response-schema.json']},
-    install_requires = ['jsonschema', 'requests'],
-    version = '0.01',
-    description = 'JSON-RPC 2.0 client library',
-    author = 'Beau Barker',
-    author_email = 'beauinmelbourne@gmail.com',
-    url = 'https://bitbucket.org/beau-barker/jsonrpcclient',
-    keywords = ['json-rpc', 'json', 'api'],
-    classifiers = [
+    install_requires=['requests', 'jsonschema'],
+    version='1.0.2',
+    description='JSON-RPC 2.0 client library',
+    author='Beau Barker',
+    author_email='beauinmelbourne@gmail.com',
+    url='https://bitbucket.org/beau-barker/jsonrpcclient',
+    keywords=['json-rpc', 'json', 'api'],
+    classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
