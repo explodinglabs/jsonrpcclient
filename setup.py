ee3f443c81bd1e0e5ce38aabb20e576d35449ea0
Beau Barker <beauinmelbourne@gmail.com>
2014-11-11 09:16:02 +1100

    setup.py: Trying setuptools instead of dist.core

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # pylint: disable=line-too-long
 """setup.py"""
 
-from distutils.core import setup
+from setuptools import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
