4219797c77e1aee572e84174001dd39c03e6ca52
Beau Barker <beauinmelbourne@gmail.com>
2014-12-19 15:11:51 +1100

    Cleaned up the exceptions, mainly the docstrings and comments

--- jsonrpcclient/exceptions.py
+++ jsonrpcclient/exceptions.py
@@ -6,59 +6,58 @@ class JsonRpcClientError(Exception):
     pass
 
 
-class ConnectionError(JsonRpcClientError): # pylint: disable=redefined-builtin
-    """The requests module raised an error, which could be a network issue,
-    invalid HTTP response or timeout. See
-    http://stackoverflow.com/questions/16511337/"""
-
-    def __init__(self):
-        super().__init__('Connection error')
-
-
 class InvalidRequest(JsonRpcClientError):
-    """The request you've sent is not valid json"""
+    """The request you're trying to send is not valid json."""
 
     def __init__(self):
         super().__init__('The request you\'re sending is not valid json')
 
 
+class ConnectionError(JsonRpcClientError): # pylint: disable=redefined-builtin
+    """The requests module raised an error, due to a network issue, invalid HTTP
+    response or timeout."""
+
+    def __init__(self):
+        super().__init__('Connection error')
+
+
 class Non200Response(JsonRpcClientError):
-    """The server responded with status code != 200"""
+    """The server responded with status code other than 200."""
 
     def __init__(self, status_code):
         super().__init__('Returned status code '+str(status_code))
 
 
 class ParseResponseError(JsonRpcClientError):
-    """Couldnt parse the json response (invalid json)"""
+    """Couldnt parse the json response."""
 
     def __init__(self):
         super().__init__('The response was not valid json')
 
 
 class InvalidResponse(JsonRpcClientError):
-    """The response didnt validate against the json-rpc response schema"""
+    """The response didnt validate against the json-rpc response schema."""
 
     def __init__(self):
         super().__init__('The response was not a valid json-rpc 2.0 response')
 
 
-class UnwantedResponse(JsonRpcClientError):
-    """The response didnt validate against the json-rpc response schema"""
-
-    def __init__(self):
-        super().__init__('The response was not asked for')
-
-
 class ReceivedNoResponse(JsonRpcClientError):
-    """A response was expected, but no response was received"""
+    """A response was expected, but none was given."""
 
     def __init__(self):
         super().__init__('No response was received')
 
 
+class UnwantedResponse(JsonRpcClientError):
+    """A response was not requested, but was given anyway."""
+
+    def __init__(self):
+        super().__init__('The response was not asked for')
+
+
 class ReceivedErrorResponse(JsonRpcClientError):
-    """The server responded with 'error'. Raise it so it must be handled"""
+    """The server responded with 'error'."""
 
     def __init__(self, code, message): #pylint:disable=unused-argument
         super().__init__(message)
