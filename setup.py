65b71e9851f8784850456cc19f821ba5e0a7fa80
Beau Barker <beauinmelbourne@gmail.com>
2015-02-06 13:27:20 +1100

    Updated documentation for version 1.1.2.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with codecs_open('HISTORY.rst', 'r', 'utf-8') as f:
 
 setup(
     name='jsonrpcclient',
-    version='1.1.1',
+    version='1.1.2',
     description='JSON-RPC 2.0 client library.',
     long_description=readme + '\n\n' + history,
     author='Beau Barker',
