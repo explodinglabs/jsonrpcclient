d8679b38c959b9f48f945d9ba5941aa6f66ba17b
Beau Barker <beauinmelbourne@gmail.com>
2015-05-09 09:05:06 +1000

    Bumped version to 1.1.6.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class Tox(TestCommand):
 
 setup(
     name='jsonrpcclient',
-    version='1.1.4',
+    version='1.1.5',
     description='JSON-RPC client library.',
     long_description=readme + '\n\n' + history,
     author='Beau Barker',
