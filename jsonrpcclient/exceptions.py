04912cfe5aecf59ae26adbfe6f15b1739b33b37c
Beau Barker <beauinmelbourne@gmail.com>
2015-07-31 16:45:25 +1000

    Restore the string passed to parent Exception class
    The value can be accessed with str(e). Re: #7

--- jsonrpcclient/exceptions.py
+++ jsonrpcclient/exceptions.py
@@ -1,44 +1,70 @@
-"""exceptions.py"""
+"""exceptions.py
+
+The string passed to the Exception class can be accessed like ``str(e)``::
+
+    >>> try:
+    ...     s.request('add', 1)
+    ... except JsonRpcClientError as e:
+    ...     print(str(e))
+    ...
+    The server gave a valid JSON-RPC error response
+    >>>
+
+"""
 
 
 class JsonRpcClientError(Exception):
-    """Base class for the other exceptions"""
+    """Base class for the other exceptions."""
     pass
 
 
 class InvalidRequest(JsonRpcClientError):
     """The request being sent is not valid JSON."""
-    pass
+    def __init__(self):
+        super(InvalidRequest, self).__init__(
+            'The request being sent is not valid json')
 
 
 class ConnectionError(JsonRpcClientError): # pylint: disable=redefined-builtin
     """There was a network issue, invalid HTTP response or timeout."""
-    pass
+    def __init__(self):
+        super(ConnectionError, self).__init__(
+            'Connection error')
 
 
 class ReceivedNoResponse(JsonRpcClientError):
     """A response message was expected, but none was given."""
-    pass
+    def __init__(self):
+        super(ReceivedNoResponse, self).__init__(
+            'No response was received')
 
 
 class UnwantedResponse(JsonRpcClientError):
     """A response was not requested, but one was given anyway."""
-    pass
+    def __init__(self):
+        super(UnwantedResponse, self).__init__(
+            'An unwanted response was given')
 
 
 class ParseResponseError(JsonRpcClientError):
     """The response was not valid json."""
-    pass
+    def __init__(self):
+        super(ParseResponseError, self).__init__(
+            'The response was not valid json')
 
 
 class InvalidResponse(JsonRpcClientError):
     """The response was not a valid JSON-RPC response."""
-    pass
+    def __init__(self):
+        super(InvalidResponse, self).__init__(
+            'The response was not a valid json-rpc 2.0 response')
 
 
 class ReceivedErrorResponse(JsonRpcClientError):
-    """The server gave a valid JSON-RPC *error* response."""
-    def __init__(self, code, message, data):
+    """The server gave a valid JSON-RPC error response."""
+    def __init__(self, code, message, data): #pylint:disable=unused-argument
+        super(ReceivedErrorResponse, self).__init__(
+            'The server gave a valid JSON-RPC error response')
         self.code = code
         self.message = message
         self.data = data
