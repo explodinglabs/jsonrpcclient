6a99ee7f69078d785b8e14056052f4fc1185b47e
Beau Barker <beauinmelbourne@gmail.com>
2015-08-13 16:08:23 +1000

    Add Server.log_request and log_response + cleanup

--- jsonrpcclient/server.py
+++ jsonrpcclient/server.py
@@ -10,19 +10,22 @@ from jsonrpcclient import rpc, exceptions
 
 
 class Server(object):
-    """This class acts as the remote server"""
-
+    """Transport-agnostic class representing the remote server. Should be
+    inherited by transport-specific subclasses."""
     # Request and response logs
     request_log = logging.getLogger(__name__+'.request')
     response_log = logging.getLogger(__name__+'.response')
 
     def __init__(self, endpoint):
-        """Instantiate a remote server object."""
+        """Instantiate a remote server object.
+
+        :param endpoint: The remote server address.
+        """
         self.endpoint = endpoint
 
     def __getattr__(self, name):
-        """Catch undefined methods and handle them as RPC requests. The
-        technique is here: http://code.activestate.com/recipes/307618/
+        """Catch undefined methods and handle them as RPC requests. Technique is
+        explained here: http://code.activestate.com/recipes/307618/
         """
         def attr_handler(*args, **kwargs):
             """Call self.request from here"""
@@ -32,20 +35,54 @@ class Server(object):
                 return self.notify(name, *args, **kwargs)
         return attr_handler
 
+    def log_request(self, request, extra=None):
+        """Log the JSON-RPC request before sending. Should be called by
+        subclasses before transporting.
+
+        :param request: The JSON-RPC request string.
+        :param extra: A dict of extra fields that may be logged.
+        """
+        extra.update({'endpoint': self.endpoint})
+        self.request_log.info(request, extra=extra)
+
+    def log_response(self, response, extra=None):
+        """Log the JSON-RPC response after sending. Should be called by
+        subclasses after transporting.
+
+        :param response: The JSON-RPC response string.
+        :param extra: A dict of extra fields that may be logged.
+        """
+        response = response.replace("\n", '').replace('  ', ' ') \
+                .replace('{ ', '{')
+        extra.update({'endpoint': self.endpoint})
+        self.response_log.info(response, extra=extra)
+
     def request(self, method_name, *args, **kwargs):
-        """JSON-RPC Request (expect a response)"""
+        """Send a JSON-RPC Request. Request means a response is expected.
+
+        :param method_name: The remote procedure's method name.
+        :param args: Positional arguments passed to the remote procedure.
+        :param kwargs: Keyword arguments passed to the remote procedure.
+        """
         kwargs['response'] = True
         return self.handle_response(
             self.send_message(rpc.request(method_name, *args, **kwargs)), True)
 
     def notify(self, method_name, *args, **kwargs):
-        """JSON-RPC Notification (no response required)"""
+        """JSON-RPC Notification. Notification means no response is expected.
+
+        :param method_name: The remote procedure's method name.
+        :param args: Positional arguments passed to the remote procedure.
+        :param kwargs: Keyword arguments passed to the remote procedure.
+        """
         return self.handle_response(
             self.send_message(rpc.request(method_name, *args, **kwargs)), False)
 
     def send_message(self, request):
-        """Send the RPC request (a json dict) to the server. Override this
-        method in the transport-specific subclass. Returns the response string.
+        """Send the RPC request to the server. Override this method in the
+        transport-specific subclass. Returns the response string.
+
+        :param request: A JSON-RPC request, in dict format.
         """
         raise NotImplementedError(
             'The Server class is now abstract; '
@@ -53,7 +90,11 @@ class Server(object):
 
     @staticmethod
     def handle_response(response, expected_response=False):
-        """Processes the response (a json string)"""
+        """Processes the response and returns the 'result' portion if present.
+
+        :param response: The JSON-RPC response string to process.
+        :param expected_response: True if we were expecting a result
+        """
         # A response was expected, but none was given
         if expected_response and not response:
             raise exceptions.ReceivedNoResponse()
