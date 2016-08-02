49a139cecb717e434ef00896cec919b2e428e1fd
Beau Barker <beauinmelbourne@gmail.com>
2016-08-02 18:38:50 +1000

    Add analytics_id code

--- doc/conf.py
+++ doc/conf.py
@@ -110,7 +110,8 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'page_width': '800px'
+    'page_width': '800px',
+    'analytics_id': 'UA-81795603-2'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
