29fbd9d32f1d69f6b14bae522f388ea0cd94ccf8
Beau Barker <beauinmelbourne@gmail.com>
2016-09-28 21:34:34 +1000

    Add AsyncClient class
    Closes #43

---
+++ jsonrpcclient/async_client.py
@@ -0,0 +1,15 @@
+from .client import Client
+from .request import Request, Notification
+
+class AsyncClient(Client):
+    async def send(self, request, **kwargs):
+        request = PreparedRequest(request)
+        self._prepare_request(request, **kwargs)
+        self._log_request(request, request.log_extra, request.log_format)
+        return await self._send_message(request, **kwargs)
+
+    async def notify(self, method_name, *args, **kwargs):
+        return await self.send(Notification(method_name, *args, **kwargs))
+
+    async def request(self, method_name, *args, **kwargs):
+        return await self.send(Request(method_name, *args, **kwargs))
