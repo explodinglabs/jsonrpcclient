9c64050c26d03877943702146f55c0c7312c0739
Beau Barker <beauinmelbourne@gmail.com>
2016-10-07 01:44:47 +1100

    Remove newlines from examples

--- examples/http_client.py
+++ examples/http_client.py
@@ -1,4 +1,3 @@
 from jsonrpcclient.http_client import HTTPClient
-
 response = HTTPClient('http://localhost:5000/').request('ping')
 print(response)
--- examples/zeromq_client.py
+++ examples/zeromq_client.py
@@ -1,4 +1,3 @@
 from jsonrpcclient.zeromq_client import ZeroMQClient
-
 response = ZeroMQClient('tcp://localhost:5000').request('ping')
 print(response)
