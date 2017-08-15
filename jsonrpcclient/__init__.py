c300453131360ef9c48586f48287d6c24b3a94a2
Beau Barker <beauinmelbourne@gmail.com>
2017-08-15 16:53:00 +1000

    Add convenience functions 'request' and 'notify'
    Closes #54

--- jsonrpcclient/__init__.py
+++ jsonrpcclient/__init__.py
@@ -1,2 +1,19 @@
 """__init__.py"""
 from .request import Request, Notification
+from .http_client import HTTPClient
+
+
+def request(endpoint, method, *args, **kwargs):
+    """
+    A convenience function. Instantiates and executes a HTTPClient request, then
+    throws it away.
+    """
+    return HTTPClient(endpoint).request(method, *args, **kwargs)
+
+
+def notify(endpoint, method, *args, **kwargs):
+    """
+    A convenience function. Instantiates and executes a HTTPClient request, then
+    throws it away.
+    """
+    return HTTPClient(endpoint).notify(method, *args, **kwargs)
