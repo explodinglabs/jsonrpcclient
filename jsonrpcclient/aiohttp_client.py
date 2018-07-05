bbb9481920d5db43c54919501a2634932a336a6b
Beau Barker <beauinmelbourne@gmail.com>
2018-07-06 08:11:19 +1000

    Include http status code and reason in aiohttp log

--- jsonrpcclient/aiohttp_client.py
+++ jsonrpcclient/aiohttp_client.py
@@ -18,5 +18,11 @@ class AiohttpClient(AsyncClient):
     async def send_message(self, request):
         with async_timeout.timeout(10):
             async with self.session.post(self.endpoint, data=request) as response:
-                response = await response.text()
-                return self.process_response(response)
+                return self.process_response(
+                    await response.text(),
+                    log_extra={
+                        "http_code": response.status,
+                        "http_reason": response.reason,
+                    },
+                    log_format="<-- %(message)s (%(http_code)s %(http_reason)s)",
+                )
