08823a33c55f68ddbdbbf4955a2f91dbaf13be5f
Beau Barker <beauinmelbourne@gmail.com>
2018-09-16 11:29:27 +1000

    Add typing to interface

--- jsonrpcclient/clients/aiohttp_client.py
+++ jsonrpcclient/clients/aiohttp_client.py
@@ -1,9 +1,12 @@
 """
 aiohttp client.
 
+Requires aiohttp >= 3.0.
+
 http://aiohttp.readthedocs.io/
 """
-from typing import Any
+from typing import Any, Optional
+from ssl import SSLContext
 
 import async_timeout  # type: ignore
 from aiohttp import ClientSession  # type: ignore
@@ -19,7 +22,12 @@ class AiohttpClient(AsyncClient):
     DEFAULT_RESPONSE_LOG_FORMAT = "<-- %(message)s (%(http_code)s %(http_reason)s)"
 
     def __init__(
-        self, session: ClientSession, endpoint: str, *args: Any, ssl=None, **kwargs: Any
+        self,
+        session: ClientSession,
+        endpoint: str,
+        *args: Any,
+        ssl: Optional[SSLContext] = None,
+        **kwargs: Any
     ) -> None:
         super().__init__(*args, **kwargs)
         self.endpoint = endpoint
@@ -44,6 +52,8 @@ class AiohttpClient(AsyncClient):
 
     async def send_message(self, request: str) -> Response:  # type: ignore
         with async_timeout.timeout(10):
-            async with self.session.post(self.endpoint, data=request, ssl=self.ssl) as response:
+            async with self.session.post(
+                self.endpoint, data=request, ssl=self.ssl
+            ) as response:
                 response_text = await response.text()
                 return Response(response_text, raw=response)
