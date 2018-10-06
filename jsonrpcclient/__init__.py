1139c4ac7ebb3907c61304f185d46e3a13a5c0c7
Beau Barker <beauinmelbourne@gmail.com>
2018-10-06 11:14:55 +1000

    Add "send" convenience function

--- jsonrpcclient/__init__.py
+++ jsonrpcclient/__init__.py
@@ -4,6 +4,12 @@ def request(endpoint, *args, **kwargs):
     from .clients.http_client import HTTPClient
     return HTTPClient(endpoint).request(*args, **kwargs)
 
+
 def notify(endpoint, *args, **kwargs):
     from .clients.http_client import HTTPClient
     return HTTPClient(endpoint).request(*args, **kwargs)
+
+
+def send(endpoint, *args, **kwargs):
+    from .clients.http_client import HTTPClient
+    return HTTPClient(endpoint).send(*args, **kwargs)
