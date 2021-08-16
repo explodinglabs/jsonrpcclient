b9f1c2dab7299340ff308b95f59de67257e80c87
Beau Barker <beau@explodinglabs.com>
2021-08-16 19:04:21 +1000

    Prepare the repo for development of v4

--- setup.py
+++ setup.py
@@ -1,35 +1,22 @@
 """setup.py"""
 from setuptools import setup  # type: ignore
 
-with open("README.md") as readme_file:
-    README = readme_file.read()
-
-test_requirements = ["mock", "pytest", "responses", "testfixtures", "requests", "pyzmq"]
-# Async requirements
-test_requirements.extend(["pytest-asyncio", "aiohttp", "tornado", "websockets"])
+with open("README.md") as f:
+    README = f.read()
 
 setup(
     author="Beau Barker",
-    author_email="beauinmelbourne@gmail.com",
+    author_email="beau@explodinglabs.com",
     classifiers=[
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Send JSON-RPC requests",
-    entry_points={"console_scripts": ["jsonrpc = jsonrpcclient.__main__:main"]},
-    extras_require={
-        "aiohttp": ["aiohttp>=3"],
-        "requests": ["requests"],
-        "requests_security": ["requests[security]"],
-        "tornado": ["tornado"],
-        "unittest": test_requirements,
-        "websockets": ["websockets"],
-        "zmq": ["pyzmq"],
-    },
     include_package_data=True,
-    install_requires=["apply_defaults<1", "click<8", "jsonschema<4"],
+    install_requires=["jsonschema<4"],
     license="MIT",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -38,7 +25,7 @@ setup(
     # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
     package_data={"jsonrpcclient": ["response-schema.json", "py.typed"]},
     zip_safe=False,
-    packages=["jsonrpcclient", "jsonrpcclient.clients"],
+    packages=["jsonrpcclient"],
     url="https://github.com/explodinglabs/jsonrpcclient",
-    version="3.3.6",
+    version="4.0.0-dev1",
 )
