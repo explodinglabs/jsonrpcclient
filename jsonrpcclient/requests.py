cb14e3f202587b5ebb32ec26a6077238e0084d95
Beau Barker <beau@explodinglabs.com>
2021-08-18 19:22:02 +1000

    Add request, request_dict, notification, notification_dict (#174)

--- jsonrpcclient/requests.py
+++ jsonrpcclient/requests.py
@@ -1,121 +1,63 @@
 import json
-from typing import Any, Callable, Iterator, Optional
+import logging
+from functools import partial
+from typing import Any, Dict, Iterator
 
 from . import id_generators
+from .sentinels import NOID
 
 
-class _RequestClassType(type):
-    """
-    Request Metaclass.
-
-    Catches undefined attributes on the class.
-    """
-
-    def __getattr__(cls: Callable, name: str) -> Callable:
-        """
-        This gives us an alternate way to make a request:
-
-        >>> Request.cat()
-        {'jsonrpc': '2.0', 'method': 'cat', 'id': 1}
-
-        That's the same as saying `Request("cat")`.
-        """
-
-        def attr_handler(*args: Any, **kwargs: Any) -> "Request":
-            return cls(name, *args, **kwargs)
-
-        return attr_handler
+def get_params(args: Any, kwargs: Any) -> Dict[str, Any]:
+    return {"params": list(args) or kwargs} if (args or kwargs) else {}
 
 
-class Notification(dict, metaclass=_RequestClassType):  # type: ignore
-    """
-    A request which does not expect a response.
-
-    >>> Notification("cat")
-    {'jsonrpc': '2.0', 'method': 'cat'}
-
-    The first argument is the *method*; everything else is *arguments* to the
-    method:
-
-    >>> Notification("cat", 'Yoko', 5)
-    {'jsonrpc': '2.0', 'method': 'cat', params: ['Yoko', 5]}
-
-    Keyword arguments are also acceptable:
-
-    >>> Notification("cat", name="Yoko", age=5)
-    {'jsonrpc': '2.0', 'method': 'cat', 'params': {'name': 'Yoko', 'age': 5}}
-
-    If you prefer, call the method as though it was a class attribute:
-
-    >>> Notification.cat(name="Yoko", age=5)
-    {'jsonrpc': '2.0', 'method': 'cat', 'params': {'name': 'Yoko', 'age': 5}}
-
-    Args:
-        method: The method name.
-        args: Positional arguments.
-        kwargs: Keyword arguments.
-
-    Returns:
-        The JSON-RPC request in dictionary form.
-    """
-
-    def __init__(self, method: str, *args: Any, **kwargs: Any) -> None:
-        super().__init__(jsonrpc="2.0", method=method)
-        # Add the params to self.
-        if args and kwargs:
-            # The 'params' can be *EITHER* "by-position" (a list) or "by-name" (a dict).
-            # Therefore, in this case it violates the JSON-RPC 2.0 specification.
-            # However, it provides the same behavior as the previous version of
-            # jsonrpcclient to keep compatibility.
-            # TODO: consider to raise a warning.
-            params_list = list(args)
-            params_list.append(kwargs)
-            self.update(params=params_list)
-        elif args:
-            self.update(params=list(args))
-        elif kwargs:
-            self.update(params=kwargs)
-
-    def __str__(self) -> str:
-        """Wrapper around request, returning a string instead of a dict"""
-        return json.dumps(self)
+def notification_dict(method: str, *args: Any, **kwargs: Any) -> Dict[str, Any]:
+    if args and kwargs:
+        logging.warning(
+            "Use positional or named arguements, but not both. This is a limitation of JSON-RPC"
+        )
+    return {"jsonrpc": "2.0", "method": method, **get_params(args, kwargs)}
 
 
-class Request(Notification):
-    """
-    Create a JSON-RPC request object
-    http://www.jsonrpc.org/specification#request_object.
+def notification(*args: Any, **kwargs: Any) -> str:
+    return json.dumps(notification_dict(*args, **kwargs))
 
-    >>> Request("cat", name="Yoko")
-    {'jsonrpc': '2.0', 'method': 'cat', 'params': {'name': 'Yoko'}, 'id': 1}
 
-    Args:
-        method: The `method` name.
-        args: Positional arguments added to `params`.
-        kwargs: Keyword arguments added to `params`. Use request_id=x to force the
-            `id` to use.
+def get_id(id: Any, id_: Any, id_generator: Iterator[Any]) -> Any:
+    if id is not NOID:
+        return id
+    elif id_ is not NOID:
+        return id_
+    else:
+        return next(id_generator)
 
-    Returns:
-        The JSON-RPC request in dictionary form.
-    """
 
-    id_generator = id_generators.decimal()
+def request_gen(
+    id_generator: Iterator[Any],
+    method: str,
+    *args: Any,
+    id: Any = NOID,
+    id_: Any = NOID,
+    **kwargs: Any,
+) -> Dict[str, Any]:
+    if args and kwargs:
+        logging.warning(
+            "Use positional or named arguements, but not both. This is a limitation of JSON-RPC"
+        )
+    request = {
+        "jsonrpc": "2.0",
+        "method": method,
+        **get_params(args, kwargs),
+    }
+    return {**request, "id": get_id(id, id_, id_generator)}
 
-    def __init__(
-        self,
-        method: str,
-        *args: Any,
-        id_generator: Optional[Iterator[Any]] = None,
-        **kwargs: Any
-    ) -> None:
-        # If 'request_id' is passed, use the specified id
-        if "request_id" in kwargs:
-            id_ = kwargs.pop("request_id", None)
-        else:  # Get the next id from the generator
-            id_generator = (
-                id_generator if id_generator is not None else self.id_generator
-            )
-            id_ = next(id_generator)
-        # We call super last, after popping the request_id
-        super().__init__(method, *args, **kwargs)
-        self.update(id=id_)
+
+request_natural_dict = partial(request_gen, id_generators.decimal())
+
+
+def request_natural(*args: Any, **kwargs: Any) -> str:
+    return json.dumps(request_natural_dict(*args, **kwargs))
+
+
+request_dict = request_natural_dict
+request = request_natural
---
+++ jsonrpcclient/sentinels.py
@@ -0,0 +1,12 @@
+import sys
+
+
+class Sentinel:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"<{sys.intern(str(self.name)).rsplit('.', 1)[-1]}>"
+
+
+NOID = Sentinel("NoId")
