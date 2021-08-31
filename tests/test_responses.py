fa78cd1b3aa29cfe2846f4a999b4bb7436b339ea
Beau Barker <beau@explodinglabs.com>
2021-08-31 16:28:59 +1000

    Bring code coverage to 100%

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,4 +1,15 @@
-from jsonrpcclient.responses import Ok, parse, parse_json
+from jsonrpcclient.responses import Error, Ok, parse, parse_json
+
+
+def test_Ok():
+    assert repr(Ok("foo", 1)) == "Ok(result='foo', id=1)"
+
+
+def test_Error():
+    assert (
+        repr(Error(1, "foo", "bar", 2))
+        == "Error(code=1, message='foo', data='bar', id=2)"
+    )
 
 
 def test_parse():
