e8ce30318d61e6990e202db272b00abd198f72aa
Beau Barker <beau@explodinglabs.com>
2022-09-16 15:44:07 +1000

    Add test section to extras_require (#207)
    Closes #206

--- setup.py
+++ setup.py
@@ -15,16 +15,22 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
     description="Send JSON-RPC requests",
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "tox",
+        ],
+    },
     include_package_data=True,
-    install_requires=[],
     license="MIT",
     long_description=README,
     long_description_content_type="text/markdown",
     name="jsonrpcclient",
-    # Be PEP 561 compliant
-    # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
-    zip_safe=False,
     packages=["jsonrpcclient"],
     url="https://github.com/explodinglabs/jsonrpcclient",
     version="4.0.2",
+    # Be PEP 561 compliant
+    # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
+    zip_safe=False,
 )
