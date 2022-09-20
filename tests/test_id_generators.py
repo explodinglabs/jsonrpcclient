f5986011548f7513bd7489a54ae279f8428af1f3
Beau Barker <beau@explodinglabs.com>
2022-09-20 14:49:37 +1000

    Change id generator test classes into functions

--- tests/test_id_generators.py
+++ tests/test_id_generators.py
@@ -4,23 +4,20 @@ from uuid import UUID
 from jsonrpcclient import id_generators
 
 
-class TestHexIterator:
-    def test(self):
-        i = id_generators.hexadecimal()
-        assert next(i) == "1"
-        i = id_generators.hexadecimal(9)
-        assert next(i) == "9"
-        assert next(i) == "a"
+def test_hexadecimal():
+    i = id_generators.hexadecimal()
+    assert next(i) == "1"
+    i = id_generators.hexadecimal(9)
+    assert next(i) == "9"
+    assert next(i) == "a"
 
 
-class TestRandomIterator:
-    def test(self):
-        i = id_generators.random()
-        assert re.match("^[0-9,a-z]{8}$", next(i))
+def test_random():
+    i = id_generators.random()
+    assert re.match("^[0-9,a-z]{8}$", next(i))
 
 
-class TestUUIDIterator:
-    def test(self):
-        i = id_generators.uuid()
-        # Raise ValueError if badly formed hexadecimal UUID string
-        UUID(next(i), version=4)
+def test_uuid():
+    i = id_generators.uuid()
+    # Raise ValueError if badly formed hexadecimal UUID string
+    UUID(next(i), version=4)
